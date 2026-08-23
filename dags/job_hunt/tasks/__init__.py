cb702fb5f9db6b68fccc2b37f108408b30c3c5e6
Andrew Vagliano <andrew.vagliano1@gmail.com>
2026-08-23 16:04:15 +0000

    remove deprecated job hunt workflow

--- dags/job_hunt/tasks/__init__.py
+++
@@ -1,7 +0,0 @@
-from .fetch_hn_jobs import fetch_hn_jobs
-from .email_digest import email_digest
-
-__all__ = [
-    "fetch_hn_jobs",
-    "email_digest",
-]
--- dags/job_hunt/utils.py
+++
@@ -1,5 +0,0 @@
-def format_job(job: dict) -> str:
-    """Render one approved listing as an HTML list item for the digest."""
-    reasons = "".join(f"<li>{reason}</li>" for reason in job["reasons"])
-
-    return f"<li><a href=\"{job['url']}\">{job['title']}</a>" f"<ul>{reasons}</ul></li>"
